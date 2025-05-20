120cc9e7e720deaa40b9b7dde69953050563dab0
toadsam <toadsam@naver.com>
2025-05-20 11:56:32 +0900

    [ADD] 몬스터 애니메이션 추가 (-재훈)

---
+++ Assets/JJH/Script/MonsterAI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAI : MonoBehaviour
+{
+    public float wanderRadius = 10f;  // 이동 반경
+    public float wanderTimer = 5f;    // 이동 주기
+
+    private NavMeshAgent agent;
+    private float timer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    // 주변에서 랜덤 위치를 찾는 함수
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+        randDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
