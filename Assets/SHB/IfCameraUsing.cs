f20564859800373ffe709c644a008cde61e9deeb
RBTylenol <sohyungbeen0403@gmail.com>
2025-06-01 15:33:38 +0900

    [ADD] 동상 분리 완료

---
+++ Assets/SHB/IfCameraUsing.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class IfCameraUsing : MonoBehaviour
+{
+    public GameObject cameraOffObject;
+    public GameObject cameraOnObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()  // 병욱이 거 구현된 거 보고 최적화 할 듯
+    {
+        /*
+        if (카메라 사용 중){
+            cameraOnObject.SetActive(false);
+            cameraOnObject.setActive(true);
+        }
+
+        else{
+            cameraOnObject.SetActive(true);
+            cameraOnObject.setActive(false);
+            }
+
+        */
+    }
+}
