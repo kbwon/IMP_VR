5b01eea2fc08acab41b7fe070528a0281198aeb0
kbwon <83692240+kbwon@users.noreply.github.com>
2025-06-14 15:29:18 +0900

    [ADD] 몬스터 콜라이더 수정

--- Assets/SHB/PlayerInfo.cs
+++ Assets/SHB/PlayerInfo.cs
@@ -33,6 +33,7 @@ public class PlayerInfo : MonoBehaviour
         DontDestroyOnLoad(gameObject); // 씬 전환 시 유지하려면
 
         keyNumberList.Add(99);
+        keyNumberList.Add(1);
     }
 
     public void printAll()
