192aaed9a64d83c92f913af17410b11077af001f
kbwon <83692240+kbwon@users.noreply.github.com>
2025-06-14 21:49:52 +0900

    [ADD] 좀비 추가

--- Assets/SHB/Pizza/FeedPizzaAtHere.cs
+++ Assets/SHB/Pizza/FeedPizzaAtHere.cs
@@ -17,6 +17,7 @@ public class FeedPizzaAtHere : MonoBehaviour
         if (!finalPizza.dough || !finalPizza.hands)
         {
             Debug.Log("피자에 햄을 넣으면 화냄");
+            PlayerInfo.Instance.isDead = true;
             onAngryEvent?.Invoke();
         }
         else
